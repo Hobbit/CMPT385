d96baf864e77f63a35447c4c34e31359d9108bc4
Chris Hobbs <chris.hobbs@hotmail.com>
2012-10-06 22:54:02 -0700

    Word Scramble update

---
+++ Word Scramble/GameIO.h
@@ -0,0 +1,13 @@
+//
+//  GameIO.h
+//  Word Scramble
+//
+//  Created by Chris on 2012-10-06.
+//  Copyright (c) 2012 HHD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameIO : NSObject
+
+@end
---
+++ Word Scramble/GameIO.m
@@ -0,0 +1,13 @@
+//
+//  GameIO.m
+//  Word Scramble
+//
+//  Created by Chris on 2012-10-06.
+//  Copyright (c) 2012 HHD. All rights reserved.
+//
+
+#import "GameIO.h"
+
+@implementation GameIO
+
+@end
